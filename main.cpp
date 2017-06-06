fbd087035d9c41c1204d0be0028548ff73cce726
pw-ethan <weilaiwen92@126.com>
2017-06-06 16:35:19 +0800

    n

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <thread>
+#include <stdio.h>
 
 using namespace std;
 
@@ -11,6 +13,11 @@ void do_something(int i) {
     cout << i << endl;
 }
 
+void do_something_in_current_thread() {
+    cout << "do something in current thread" << endl;
+    throw;
+}
+
 class background_task {
 public:
     void operator()() const {
@@ -20,9 +27,10 @@ public:
 
 struct func {
     int& i;
-    func(int& i_) : i(i_) {}
+    func(int& i_) : i(i_) { cout << "func()" << endl; }
+    ~func() { cout << "~func()" << endl; }
     void operator() () {
-        for (unsigned j = 0; j < 1000; ++j) {
+        for (unsigned j = 0; j < 10; ++j) {
             do_something(i);
             cout << j << endl;
         }
@@ -34,6 +42,7 @@ class thread_guard {
 public:
     explicit thread_guard(thread& t_) : t(t_) {}
     ~thread_guard() {
+        cout << "~thread_guard()" << endl;
         if (t.joinable()) {
             cout << "join in ~thread_guard()" << endl;
             t.join();
@@ -43,24 +52,86 @@ public:
     thread_guard& operator=(thread_guard const&) = delete;
 };
 
-double mul(double x, double y) {
-    if (y == 0) {
-        throw y;
-    }
-    return x / y;
-}
-
 void f() {
     int some_local_state = 0;
     func my_func(some_local_state);
     thread t(my_func);
     thread_guard g(t);
-    cout << mul(2.1, 0) << endl;
+    do_something_in_current_thread();
+}
+
+void f1(int i, const string s) {
+    cout << "h" << endl;
+    cout << i << " " << s << endl;
+}
+
+void not_oops(int some_param) {
+    char buffer[1024];
+    sprintf(buffer, "%i", some_param);
+    thread t(f1, 3, string(buffer));
+    t.detach();
+}
+
+class widget_data {
+public:
+    int data;
+    widget_data(int d = 0) : data(d) { }
+    widget_data(const widget_data& rhs) {
+        data = rhs.data;
+        cout << "copy constructor" << endl;
+    }
+    widget_data& operator=(const widget_data& rhs) {
+        if (this != &rhs) {
+            data = rhs.data;
+        }
+        cout << "operator=" << endl;
+        return *this;
+    }
+    ~widget_data() {
+        cout << "~widget_data()" << endl;
+    }
+    void operator() () {
+        cout << "operator()" << data << endl;
+    }
+    void show(int n) {
+        cout << "show() " << data << " " << n << endl;
+    }
+};
+
+void update_data_for_widget(widget_data& w) {
+    cout << w.data << endl;
+    w.data = 1;
+}
+
+void oops_again() {
+    widget_data w(0);
+    thread t(update_data_for_widget, ref(w));
+    cout << w.data << endl;
+    t.join();
+    cout << w.data << endl;
+}
+
+void oops() {
+    widget_data w(2);
+    int num(3);
+    thread t(&widget_data::show, &w, num);
+    t.join();
+}
+
+void process_widget(unique_ptr<widget_data> p) {
+    p->data = 12;
+    p->show(1);
+}
+
+void func_ptra() {
+    unique_ptr<widget_data> p(new widget_data);
+    p->data = 10;
+    thread t(process_widget, move(p));
+    t.join();
 }
 
 int main()
 {
-    f();
-
+    func_ptra();
     return 0;
 }
